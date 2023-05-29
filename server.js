ecc5cbd1d72baa99815c98cfd24562d71d149003
Hosam <108076964+Hosam89@users.noreply.github.com>
2023-05-29 16:51:28 +0200

    like controller done

--- server.js
+++ server.js
@@ -1,29 +1,27 @@
 // import dependencies
-const bcrypt = require('bcrypt');
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const cors = require('cors');
-require('dotenv').config();
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
-const multer = require('multer');
-const { multerStorageCloudinary } = require('multer-storage-cloudinary');
-const socketio = require('socket.io');
+const bcrypt = require('bcrypt')
+const cloudinary = require('cloudinary').v2
+const { CloudinaryStorage } = require('multer-storage-cloudinary')
+const cors = require('cors')
+require('dotenv').config()
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
+const multer = require('multer')
+const { multerStorageCloudinary } = require('multer-storage-cloudinary')
+const socketio = require('socket.io')
 
 // import routes (modules)
-const userRoute = require('./routes/userRoute');
-const postRoute = require('./routes/postRoute');
+const userRoute = require('./routes/userRoute')
+const postRoute = require('./routes/postRoute')
 
-const app = express(); // initialize express
+const app = express() // initialize express
 
 // import middlewares
-const { connectMongoDB } = require('./lib/mongoose'); // destruct mongoDB connector
-const { errorHandler } = require('./middlewares/errorHandler'); // destruct errorHandler
+const { connectMongoDB } = require('./lib/mongoose') // destruct mongoDB connector
+const { errorHandler } = require('./middlewares/errorHandler') // destruct errorHandler
 
-const { PORT, FRONTEND_URL } = process.env; // destruct envs
-
-
+const { PORT, FRONTEND_URL } = process.env // destruct envs
 
 // COMMENTED OUT CONNECTION FOR FALLBACK IF CONNECTION HANDLER FAILS
 // // atlas connection string
@@ -43,36 +41,36 @@ const { PORT, FRONTEND_URL } = process.env; // destruct envs
 // };
 // main();
 
-
-
 // enable cross-origin resource sharing for the express app
-app.use(cors());
+app.use(cors())
 
 // allow requests from specified origins with specific methods
-app.use(
-    cors({
-      origin: FRONTEND_URL,
-      methods: "GET,POST,PUT,DELETE",
+const whitelist = ['http://localhost:3000', 'https://www.arii.me']
 
-    // enable reception of cookies and other auth credentials in req/res
-      credentials: true,
-    })
-  );
-
-  // enable parsing of incoming JSON data in the request body by the express app
-app.use(express.json());
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('CORS issues'))
+    }
+  },
+  credentials: true,
+}
+app.use(cors(corsOptions))
+// enable parsing of incoming JSON data in the request body by the express app
+app.use(express.json())
 
 // define routes
-app.use('/users', userRoute);
-app.use('/posts', postRoute);
+app.use('/users', userRoute)
+app.use('/posts', postRoute)
 // app.use('/auth', authRoute);
 
-
 // define middlewares
-connectMongoDB();
-app.use(errorHandler); // error handler must be last invoked middleware
+connectMongoDB()
+app.use(errorHandler) // error handler must be last invoked middleware
 
 // listen to server
 app.listen(PORT || 3003, () => {
-    console.log(`Server up and running at ${PORT}`);
-});
+  console.log(`Server up and running at ${PORT}`)
+})
